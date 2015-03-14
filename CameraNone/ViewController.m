ac71c5c04f8f7ac121efc3f93e88cf6e0ea0457c
jhihguan <imdiedie@gmail.com>
2015-03-14 20:44:10 +0800

    add zoom support

--- CameraNone/ViewController.m
+++ CameraNone/ViewController.m
@@ -7,9 +7,22 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
+@property (nonatomic, strong)       AVCaptureSession            * session;
+
+@property (nonatomic, strong)       AVCaptureDeviceInput        * videoInput;
+
+@property (nonatomic, strong)       AVCaptureStillImageOutput   * stillImageOutput;
+
+@property (nonatomic, strong)       AVCaptureVideoPreviewLayer  * previewLayer;
+
+
+// for scaling
+@property (nonatomic) CGFloat lastScale;
+@property (nonatomic) CGFloat effectiveScale;
 @end
 
 @implementation ViewController
@@ -17,6 +30,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    // create camera vc
+    [self initSession];
+    self.effectiveScale = 1.0f;
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    
+    UIView * view = self.view;
+    CALayer * viewLayer = [view layer];
+    [viewLayer setMasksToBounds:YES];
+    
+    CGRect bounds = [view bounds];
+    [self.previewLayer setFrame:bounds];
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+    [viewLayer insertSublayer:self.previewLayer below:[[viewLayer sublayers] objectAtIndex:0]];
+    
+    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGesture:)];
+    [self.view addGestureRecognizer:pinchGestureRecognizer];
+}
+
+- (void)handlePinchGesture:(UIPinchGestureRecognizer *)gestureRecognizer {
+    
+    if([gestureRecognizer state] == UIGestureRecognizerStateBegan) {
+        self.lastScale = self.effectiveScale;
+    }
+    
+    self.effectiveScale = self.lastScale * gestureRecognizer.scale;
+    if (self.effectiveScale < 1.0) {
+        self.effectiveScale = 1.0;
+    }
+    CGFloat maxScaleAndCropFactor = [[self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo] videoMaxScaleAndCropFactor];
+    if (self.effectiveScale > maxScaleAndCropFactor) {
+        self.effectiveScale = maxScaleAndCropFactor;
+    }
+    [CATransaction begin];
+    [CATransaction setAnimationDuration:.025];
+    [self.previewLayer setAffineTransform:CGAffineTransformMakeScale(self.effectiveScale, self.effectiveScale)];
+    [CATransaction commit];
+};
+
+- (void)initSession {
+    self.session = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *videoDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    self.videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:videoDevice error:nil];
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary * outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG,AVVideoCodecKey, nil];
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    
+    if ([self.session canAddInput:self.videoInput]) {
+        [self.session addInput:self.videoInput];
+    }
+    if ([self.session canAddOutput:self.stillImageOutput]) {
+        [self.session addOutput:self.stillImageOutput];
+    }
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (self.session) {
+        [self.session startRunning];
+    }
+}
+
+- (void) viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear: animated];
+    if (self.session) {
+        [self.session stopRunning];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
